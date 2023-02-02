662cdf480fb53284a03fbc6b19bc328c60ba55a7
Andrew Kim <jeesoo2002@gmail.com>
2023-02-02 23:38:57 +0900

    question 11720 done

--- app.js
+++ app.js
@@ -3,28 +3,12 @@
 
 //한 줄 입력
 let fs = require('fs');
-let input = fs.readFileSync('./input.txt').toString().split('');
+let input = fs.readFileSync('./input.txt').toString().split('\n');
 
 let sum = 0;
 
-const dial = {
-  2: 'ABC',
-  3: 'DEF',
-  4: 'GHI',
-  5: 'JKL',
-  6: 'MNO',
-  7: 'PQRS',
-  8: 'TUV',
-  9: 'WXYZ',
-};
-
-for (let i = 0; i < input.length; i++) {
-  for (let j = 2; j <= 9; j++) {
-    if (dial[j].includes(input[i])) {
-      sum += j + 1;
-      break;
-    }
-  }
+for (let i = 0; i < Number(input[0]); i++) {
+  sum += Number(input[1][i]);
 }
 
 console.log(sum);
