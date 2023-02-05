78153567284d725aab919cd9cd3c8f0522cd9a2a
Andrew Kim <jeesoo2002@gmail.com>
2023-02-06 00:27:49 +0900

    question 2941 done

---
+++ 2941/app.js
@@ -0,0 +1,14 @@
+// 백준 route
+// let input = fs.readFileSync('/dev/stdin').toString().split(' ');
+
+//한 줄 입력
+let fs = require('fs');
+let input = fs.readFileSync('./input.txt').toString().trim();
+
+const croAlpha = ['c=', 'c-', 'dz=', 'd-', 'lj', 'nj', 's=', 'z='];
+
+for (let alpha of croAlpha) {
+  input = input.split(alpha).join('Q');
+}
+
+console.log(input.length);
