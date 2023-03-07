ce0cf3d56ad95cd2c13391a2bde90e252983f8e5
Andrew Kim <jeesoo2002@gmail.com>
2023-03-08 02:06:46 +0900

    3003: 킹, 퀸, 룩, 비숍, 나이트, 폰 done

---
+++ 3003/app.js
@@ -0,0 +1,16 @@
+// 백준 route
+// let input = fs.readFileSync('/dev/stdin').toString().split(' ');
+
+//한 줄 입력
+let fs = require('fs');
+let input = fs.readFileSync('./input.txt').toString().split(' ');
+
+const original = [1, 1, 2, 2, 2, 8];
+const diff_pcs = [];
+
+for (let i = 0; i < input.length; i++) {
+  const diff = original[i] - input[i];
+  diff_pcs.push(diff);
+}
+
+console.log(diff_pcs.join(' '));
--- app.js
+++ app.js
@@ -3,13 +3,10 @@
 
 //한 줄 입력
 let fs = require('fs');
-let input = fs.readFileSync('./input.txt').toString().trim();
+let input = fs.readFileSync('./input.txt').toString().split('\n');
 
-console.log(input);
-const result = [];
+console.log(input[0]);
 
-for (let i = 97; i <= 122; i++) {
-  result.push(input.indexOf(String.fromCharCode(i)));
+for (let i = 1; i <= Number(input[0]); i++) {
+  console.log(input[i][0] + input[i][input[i].length - 1]);
 }
-
-console.log(result.join(' '));
