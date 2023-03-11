a67a569efcb1465910bb49dd18154e38fa0981c5
Andrew Kim <jeesoo2002@gmail.com>
2023-03-12 01:55:46 +0900

    1157: 단어 공부 done

---
+++ 1157/app.js
@@ -0,0 +1,27 @@
+// 백준 route
+// let fs = require('fs');
+// let input = fs.readFileSync('/dev/stdin').toString().split(' ');
+
+//한 줄 입력
+let fs = require('fs');
+let input = fs.readFileSync('./input.txt').toString().trim().toLowerCase();
+
+const result = new Array(26).fill(0);
+
+for (let i = 0; i < input.length; i++) {
+  result[input.charCodeAt(i) - 97]++;
+}
+
+const max = Math.max(...result);
+const index = result.indexOf(max);
+
+let isSame = false;
+
+for (let j = 0; j < 26; j++) {
+  if (result[j] === max && index !== j) {
+    isSame = true;
+    break;
+  }
+}
+
+console.log(isSame ? '?' : String.fromCharCode(index + 65));
