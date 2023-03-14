7af8894126872f6c60567c2deb686b090498df29
Andrew Kim <jeesoo2002@gmail.com>
2023-03-15 01:39:50 +0900

    question 1316: 그룹단어 체크 done

--- 1316/app.js
+++ 1316/app.js
@@ -5,27 +5,26 @@
 let fs = require('fs');
 let input = fs.readFileSync('./input.txt').toString().split('\n');
 
-const wordlength = Number(input[0]);
-let groupWordNum = 0;
+const wordCount = Number(input[0]);
+let count = 0;
 
-for (let i = 1; i <= wordlength; i++) {
-  const currentWord = input[i]; // happy;
-  const arr = [];
+for (let i = 1; i <= wordCount; i++) {
+  const word = input[i];
   let isGroupWord = true;
+  const result = [];
 
-  for (let j = 0; j < currentWord.length; j++) {
-    // arr에서 현재 캐릭터를 못찾으면 arr에 추가
-    if (arr.indexOf(currentWord[j]) === -1) {
-      arr.push(currentWord[j]);
-    } else if (arr.indexOf(currentWord[j]) !== arr.length - 1) {
+  for (let j = 0; j < word.length; j++) {
+    if (result.indexOf(word[j]) === -1) {
+      result.push(word[j]);
+    } else if (result.indexOf(word[j]) !== result.length - 1) {
       isGroupWord = false;
       break;
     }
   }
 
   if (isGroupWord) {
-    groupWordNum += 1;
+    count++;
   }
 }
 
-console.log(groupWordNum);
+console.log(count);
